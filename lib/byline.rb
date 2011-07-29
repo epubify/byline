8693e8abbfdc4fe59d5dee69eee83a2d1096dd7f
Rune Myrland <rune@epubify.com>
2011-07-29 06:24:24 +0200

    Rails engine

--- lib/byline.rb
+++ lib/byline.rb
@@ -1,3 +1,13 @@
-require "app/models/gravatar.rb"
-require "app/models/byline.rb"
+require 'active_model'
+require 'simple_record'
+
+module BylineEngine
+  class Engine < Rails::Engine
+
+    initializer 'simpledb connection' do |app|
+      SimpleRecord.establish_connection(ENV['SIMPLEDB_KEY_ID'], ENV['SIMPLEDB_KEY'], :connection_mode => :per_thread, :server => ENV['SIMPLEDB_SERVER'])
+    end
+
+  end
+end
 
